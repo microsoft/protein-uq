488cb5eb0a95e9b61228b05f4b30966d065a08a5
Kevin Greenman <kpg@mit.edu>
2022-01-12 19:58:01 -0500

    added training, testing, output for GP

--- src/models/gp.py
+++ src/models/gp.py
@@ -2,8 +2,37 @@ import math
 import torch
 import gpytorch
 
-train_x = 
-train_y = 
+import argparse
+from sklearn.linear_model import BayesianRidge
+from sklearn.preprocessing import StandardScaler
+
+import torch
+import pandas as pd
+import numpy as np
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+
+from train_all import split_dict 
+from utils import SequenceDataset, load_dataset, calculate_metrics
+from csv import writer
+from pathlib import Path
+
+class Tokenizer(object):
+    """Convert between strings and their one-hot representations."""
+    def __init__(self, alphabet: str):
+        self.alphabet = alphabet
+        self.a_to_t = {a: i for i, a in enumerate(self.alphabet)}
+        self.t_to_a = {i: a for i, a in enumerate(self.alphabet)}
+
+    @property
+    def vocab_size(self) -> int:
+        return len(self.alphabet)
+
+    def tokenize(self, seq: str) -> np.ndarray:
+        return np.array([self.a_to_t[a] for a in seq])
+
+    def untokenize(self, x) -> str:
+        return ''.join([self.t_to_a[t] for t in x])
 
 class ExactGPModel(gpytorch.models.ExactGP):
     def __init__(self, train_x, train_y, likelihood):
@@ -15,6 +44,75 @@ class ExactGPModel(gpytorch.models.ExactGP):
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+
+np.random.seed(0)
+torch.manual_seed(1)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('dataset', type=str, help='file path to data directory')
+parser.add_argument('task', type=str)
+args = parser.parse_args()
+
+AAINDEX_ALPHABET = 'ARNDCQEGHILKMFPSTWYVXU'
+# grab data
+split = split_dict[args.task]
+train, test, _ = load_dataset(args.dataset, split+'.csv', val_split=False)
+
+ds_train = SequenceDataset(train)
+ds_test = SequenceDataset(test)
+
+# tokenize train data
+all_train = list(ds_train)
+X_train = [i[0] for i in all_train]
+y_train = [i[1] for i in all_train]
+if args.dataset == 'meltome':
+    AAINDEX_ALPHABET += 'XU'
+tokenizer = Tokenizer(AAINDEX_ALPHABET) # tokenize
+X_train = [torch.tensor(tokenizer.tokenize(i)).view(-1, 1) for i in X_train]
+
+
+# tokenize test data
+all_test = list(ds_test)
+X_test = [i[0] for i in all_test]
+y_test = [i[1] for i in all_test]
+tokenizer = Tokenizer(AAINDEX_ALPHABET) # tokenize
+X_test = [torch.tensor(tokenizer.tokenize(i)).view(-1,1) for i in X_test]
+
+
+# padding
+maxlen_train = max([len(i) for i in X_train])
+maxlen_test = max([len(i) for i in X_test])
+maxlen = max([maxlen_train, maxlen_test])
+# pad_tok = alphabet.index(PAD)
+
+X_train = [F.pad(i, (0, 0, 0, maxlen - i.shape[0]), "constant", 0.) for i in X_train]
+X_train_enc = [] # ohe
+for i in X_train:
+    i_onehot = torch.FloatTensor(maxlen, len(AAINDEX_ALPHABET))
+    i_onehot.zero_()
+    i_onehot.scatter_(1, i, 1)
+    X_train_enc.append(i_onehot)
+X_train_enc = np.array([np.array(i.view(-1)) for i in X_train_enc]) # flatten
+
+X_test = [F.pad(i, (0, 0, 0, maxlen - i.shape[0]),"constant", 0.) for i in X_test]
+X_test_enc = [] # ohe
+for i in X_test:
+    i_onehot = torch.FloatTensor(maxlen, len(AAINDEX_ALPHABET))
+    i_onehot.zero_()
+    i_onehot.scatter_(1, i, 1)
+    X_test_enc.append(i_onehot)
+X_test_enc = np.array([np.array(i.view(-1)) for i in X_test_enc]) # flatten
+
+
+# scale X
+if args.scale:
+    scaler = StandardScaler()
+    X_train_enc = scaler.fit_transform(X_train_enc)
+    X_test_enc = scaler.transform(X_test_enc)
+
+
+train_x, train_y = X_train_enc, y_train
+test_x, test_y = X_test_enc, y_test
 
 # initialize likelihood and model
 likelihood = gpytorch.likelihoods.GaussianLikelihood()
@@ -51,7 +149,7 @@ for i in range(training_iter):
     ))
     optimizer.step()
 
-test_x = 
+
 test_x = test_x.cuda()
 
 # Get into evaluation (predictive posterior) mode
@@ -63,12 +161,32 @@ likelihood.eval()
 with torch.no_grad(), gpytorch.settings.fast_pred_var():
     observed_pred = likelihood(model(test_x))
     mean = observed_pred.mean
-    lower, upper = observed_pred.confidence_region()
+    lower, upper = observed_pred.confidence_region() # 2 standard deviations above and below mean
 
-mean = mean.cpu()
+preds_mean = mean.cpu()
 lower = lower.cpu()
 upper = upper.cpu()
+preds_std = (upper-preds_mean)/2
 
 train_x = train_x.cpu()
 train_y = train_y.cpu()
 test_x = test_x.cpu()
+
+# def main(args, X_train_enc, y_train, y_test):
+
+#     # print('Parameters...')
+#     # print('Solver: %s, MaxIter: %s, Tol: %s' % (args.solver, args.max_iter, args.tol))
+
+#     print('Training...')
+    
+
+print('Calculating metrics...')
+algorithm_type = 'GPcontinuous'
+metrics = calculate_metrics(y_test, preds_mean, preds_std, args, split, y_train, algorithm_type)
+
+# Write metric results to file
+row = [args.dataset, algorithm_type, split]
+for metric in metrics:
+    row.append(round(metric, 2))
+with open(Path.cwd() / 'evals_new'/ (args.dataset+'_results.csv'), 'a', newline='') as f:
+    writer(f).writerow(row)
