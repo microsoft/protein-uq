438f0f2a73d788964241e6686754d33a7e1a62c8
Kevin Greenman <kpg@mit.edu>
2022-06-24 23:46:16 -0400

    created symbolic link to embeddings dir in FLIP

--- src/models/filepaths.py
+++ src/models/filepaths.py
@@ -1,3 +1,3 @@
 HOME_DIR = "../../"  # protein-uq home directory
 DATA_DIR = HOME_DIR + "data/"
-EMBEDDING_DIR = "../../../FLIP/baselines/embeddings/"  # TODO: move these to within the protein-uq repo and update path here
+EMBEDDING_DIR = DATA_DIR + "embeddings/"
